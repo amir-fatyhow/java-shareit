af272964916dcd21830324696050546d57515179
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-10-06 21:06:55 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -15,6 +15,7 @@ import java.util.Map;
 @RestControllerAdvice(assignableTypes = {BookingController.class, UserController.class, ItemController.class})
 public class ErrorHandler {
     public static final String ERROR = "error";
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNullPointerException(final NullPointerException e) {
