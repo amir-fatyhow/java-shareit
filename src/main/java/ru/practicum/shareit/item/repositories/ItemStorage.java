8bd749fc3bfc1c64bf4aceb97bfe47cae46061dc
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-10-06 21:43:38 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/item/repositories/ItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/repositories/ItemStorage.java
@@ -2,10 +2,12 @@ package ru.practicum.shareit.item.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
+@Repository
 public interface ItemStorage extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerId(long ownerId);
 
