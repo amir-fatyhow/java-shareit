3ac1813bf26fcc5989e848c0a695ab68dd57ca3e
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-10-31 22:05:06 +0300

    Delete src directory

--- src/main/java/ru/practicum/shareit/user/exceptions/UserNotBooker.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.user.exceptions;
-
-public class UserNotBooker extends RuntimeException {
-    public UserNotBooker(String message, long userId) {
-        super(message);
-    }
-}
