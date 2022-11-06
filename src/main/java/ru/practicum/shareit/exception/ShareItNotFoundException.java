17fdb0083589d6589d6c4ec2f174285d7d0d607f
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-11-06 18:13:35 +0300

    Delete src/main directory

--- src/main/java/ru/practicum/shareit/exception/ShareItNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exception;
-
-//404
-public class ShareItNotFoundException extends RuntimeException {
-
-    public ShareItNotFoundException(String message) {
-        super(message);
-    }
-}
