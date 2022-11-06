08da4b1f900e78c7da1906857a2ceba4a2b1b5b7
amir-fatyhow <100147741+amir-fatyhow@users.noreply.github.com>
2022-11-06 18:59:44 +0300

    Delete gateway directory

--- gateway/src/main/java/ru/practicum/shareit/exception/ShareItNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class ShareItNotFoundException extends RuntimeException {
-
-    public ShareItNotFoundException(String message) {
-        super(message);
-    }
-}
